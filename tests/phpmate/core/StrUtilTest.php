03e94ff8ba4ebb87caa8078dd70544be1ea14e3d
shali <shali@gmail.com>
2025-05-07 16:48:59 +0800

    feat: 添加字符串工具类

---
+++ tests/phpmate/core/StrUtilTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace shali\phpmate\tests\core;
+
+use PHPUnit\Framework\TestCase;
+use shali\phpmate\core\StrUtil;
+
+class StrUtilTest extends TestCase
+{
+    /**
+     * @test 测试数组转成 & 拼接，字典序
+     */
+    function httpBuildQuery()
+    {
+        $params = [
+            'b' => '2',
+            'a' => '1',
+            'c' => '3',
+        ];
+        self::assertEquals('b=2&a=1&c=3', StrUtil::httpBuildQuery($params));
+        $this->assertEquals('a=1&b=2&c=3', StrUtil::httpBuildQuery($params, true));
+    }
+}
