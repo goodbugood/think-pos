56605d4aa9dc001a4edb3bb9aeda39039cec119d
shali <shali@gmail.com>
2025-09-19 09:32:51 +0800

    refactor: 提现状态枚举类新增语义更明确的等待确认状态

--- src/constant/WithdrawStatus.php
+++ src/constant/WithdrawStatus.php
@@ -5,12 +5,18 @@ namespace think\pos\constant;
 /**
  * 提现状态常量
  */
-interface WithdrawStatus
+final class WithdrawStatus
 {
     /**
      * 待处理
+     * @deprecated 废弃，不明确的语义，请使用 WAIT_CONFIRM
      */
     const INIT = 'INIT';
+
+    /**
+     * 待处理
+     */
+    const WAIT_CONFIRM = 'WAIT_CONFIRM';
 
     /**
      * 处理中
@@ -33,4 +39,13 @@ interface WithdrawStatus
     const FAIL = 'FAIL';
 
     const REJECT = 'REJECT';
+
+    // 私有构造和 clone
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
 }
